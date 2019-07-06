1fcac8dce6754a956c17045c930c2f618c09bc93
trepichio <trepichio@gmail.com>
2019-07-06 16:25:32 -0300

    add header component (prototyping idea)

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+	  <p>
+      <strong>Fancy Quiz App</strong>
+    </p>
+    <p>Counter: 2/10</p>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  div {
+    width: 50%;
+    text-align: left;
+  }
+  p {
+    display: inline-block;
+    margin: 0 15px;
+  }
+</style>
