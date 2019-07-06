37ce6382eb8091e01f2b9190f6a8916ae1762583
trepichio <trepichio@gmail.com>
2019-07-06 16:29:58 -0300

    add answer list and its style (prototyping idea)

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -3,6 +3,12 @@
 		<h1>Questions here?</h1>
 		<hr>
 		<p>Answers List here</p>
+		<ul>
+			<li>Answer 1</li>
+			<li>Answer 2</li>
+			<li>Answer 3</li>
+			<li>Answer 4</li>
+		</ul>
 	</div>
 </template>
 
@@ -16,5 +22,13 @@ export default {
 </script>
 
 <style scoped>
-	
+	ul {
+		width: 50%;
+		margin: 0 auto;
+		list-style-type: none;
+	}
+
+	li {
+		padding: 5px;
+	}
 </style>
