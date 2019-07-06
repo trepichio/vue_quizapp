933df77180057f1e6ca33c7138caa80d224e8152
trepichio <trepichio@gmail.com>
2019-07-06 16:30:28 -0300

    add Header and QuestionBox components (prototyping idea)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-
+    <Header />
+    <QuestionBox />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    
+    Header,
+    QuestionBox
   }
 }
 </script>
