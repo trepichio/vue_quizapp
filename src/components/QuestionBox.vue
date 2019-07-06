165daad7dba72391639a46778a3e4e5a4bf02e71
trepichio <trepichio@gmail.com>
2019-07-06 16:18:17 -0300

    prototyping main layout idea

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>Questions here?</h1>
+		<hr>
+		<p>Answers List here</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'QuestionBox'
+  
+}
+</script>
+
+<style scoped>
+	
+</style>
